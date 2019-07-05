90b7f9684ac7febfb7f68fb5678ac5eb78a0d8bc
Hitendra Kumar <hitendra1632@gmail.com>
2019-07-05 13:52:06 +0800

    adding require all gem for adding all files

--- lib/common_app.rb
+++ lib/common_app.rb
@@ -2,17 +2,7 @@ require 'rest-client'
 require 'savon'
 require 'require_all'
 
-require "common_app/configuration"
-require "common_app/rest_api"
-require "common_app/soap_service"
-require "common_app/version"
-require "common_app/auth/base"
-require "common_app/auth/rest_token"
-require "common_app/auth/soap_token"
-require "common_app/create/applicant"
-require "common_app/get/applicant"
-require "common_app/get/members"
-require "common_app/post/counselor/secondary_report"
+require_all 'lib'
 
 module CommonApp
   class << self
