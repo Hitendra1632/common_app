426888eab5f8798c706b60346556dc5c1f168f87
Ather Mohiuddin <ather@cialfo.com.sg>
2021-05-26 00:24:52 +0500

    update school report api post and submit call

--- lib/common_app/post/counselor/secondary_report.rb
+++ lib/common_app/post/counselor/secondary_report.rb
@@ -2,25 +2,9 @@ module CommonApp
   module Post
     module Counselor
       # for sending SecondaryReport
-      class SecondaryReport
-        attr_reader :params
-
-        def initialize(params)
-          @params = params
-        end
-
-        def call
-          submitted_form
-        end
-
-        private
-
-        def submitted_form
-          SoapService.new(operation, params).call
-        end
-
-        def operation
-          :post_form_counselor_secondary_report
+      class SecondaryReport < Base
+        def endpoint
+          "counselor/secondary-report"
         end
       end
     end
