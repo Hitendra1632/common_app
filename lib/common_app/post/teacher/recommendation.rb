610f262ce6bbf9e96ddad1b5d543a270e085895c
Ather Mohiuddin <ather@cialfo.com.sg>
2021-05-26 00:34:51 +0500

    update teacher eval post and submit api calls

--- lib/common_app/post/teacher/recommendation.rb
+++ lib/common_app/post/teacher/recommendation.rb
@@ -2,25 +2,9 @@ module CommonApp
   module Post
     module Teacher
       # for sending Recommendation
-      class Recommendation
-        attr_reader :params
-
-        def initialize(params)
-          @params = params
-        end
-
-        def call
-          submitted_form
-        end
-
-        private
-
-        def submitted_form
-          SoapService.new(operation, params).call
-        end
-
-        def operation
-          :post_form_teacher_recommendation
+      class Recommendation < Base
+        def endpoint
+          "teacher/recommendation"
         end
       end
     end
