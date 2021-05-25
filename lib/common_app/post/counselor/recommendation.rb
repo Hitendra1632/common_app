9270c6fcf868170fbc05bc0522a9c9d664b4715d
Ather Mohiuddin <ather@cialfo.com.sg>
2021-05-26 00:22:12 +0500

    update post and submit recommendation classes

--- lib/common_app/post/counselor/recommendation.rb
+++ lib/common_app/post/counselor/recommendation.rb
@@ -2,31 +2,9 @@ module CommonApp
   module Post
     module Counselor
       # for sending Recommendation
-      class Recommendation
-        attr_reader :params, :recommender_id, :applicant_id
-
-        def initialize(recommender_id, applicant_id, params)
-          @params = params
-          @recommender_id = recommender_id
-          @applicant_id   = applicant_id
-        end
-
-        def call
-          post
-        end
-
-        private
-
-        def method
-          'post'
-        end
-
+      class Recommendation < Base
         def endpoint
           "counselor/recommendation"
-        end
-
-        def post
-          RestApi.new(method, endpoint, params.merge(applicantId: applicant_id, recommenderId: recommender_id)).call
         end
       end
     end
--- lib/common_app/submit/counselor/recommendation.rb
+++ lib/common_app/submit/counselor/recommendation.rb
@@ -3,36 +3,8 @@ module CommonApp
     module Counselor
       # for submit Recommendation
       class Recommendation
-        attr_reader :recommender_id, :applicant_id
-
-        def initialize(recommender_id, applicant_id)
-          @recommender_id = recommender_id
-          @applicant_id   = applicant_id
-        end
-
-        def call
-          submit
-        end
-
-        private
-
-        def method
-          'post'
-        end
-
         def endpoint
           "counselor/submit/recommendation"
-        end
-
-        def params
-          {
-            applicantId: applicant_id,
-            recommenderId: recommender_id
-          }
-        end
-
-        def submit
-          RestApi.new(method, endpoint, params).call
         end
       end
     end
