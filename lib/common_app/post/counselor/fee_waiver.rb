116212acc44274430c4f066c73f45110ad7905fc
Ather Mohiuddin <ather@cialfo.com.sg>
2021-05-27 14:36:28 +0500

    update ed, ed2, fee waiver api calls

--- lib/common_app/post/counselor/fee_waiver.rb
+++ lib/common_app/post/counselor/fee_waiver.rb
@@ -2,27 +2,11 @@ module CommonApp
   module Post
     module Counselor
       # for sending Fee Waiver
-      class FeeWaiver
-        attr_reader :params
-
-        def initialize(params)
-          @params = params
-        end
-
-        def call
-          submitted_form
-        end
-
-        private
-
-        def submitted_form
-          SoapService.new(operation, params).call
-        end
-
-        def operation
-          :post_form_counselor_fee_waiver
+      class FeeWaiver < Base
+        def endpoint
+          "counselor/fee-waiver"
         end
       end
     end
-  end  
+  end
 end
