47d63780a7aed7051d44be3783191042c060a607
Ather Mohiuddin <ather@cialfo.com.sg>
2021-08-16 14:44:03 +0500

    fix issues

--- lib/common_app/get/base.rb
+++ lib/common_app/get/base.rb
@@ -1,18 +1,16 @@
 module CommonApp
   module Get
-    module Counselor
-      class Base
-        def call
-          get
-        end
+    class Base
+      def call
+        get
+      end
 
-        def method
-          'get'
-        end
+      def method
+        'get'
+      end
 
-        def get
-          RestApi.new(method, endpoint).call
-        end
+      def get
+        RestApi.new(method, endpoint).call
       end
     end
   end
