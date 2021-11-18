4e5c4fa610ec7228f961486a344e23f873a1d471
Ather Mohiuddin <ather@cialfo.com.sg>
2021-11-19 04:04:01 +0500

    add post and submit services for optional report 2

---
+++ lib/common_app/post/counselor/optional_report2.rb
@@ -0,0 +1,12 @@
+module CommonApp
+  module Post
+    module Counselor
+      # for sending  Optional Report
+      class OptionalReport2 < Base
+        def endpoint
+          "recommender/counselor/optional-report-2"
+        end
+      end
+    end
+  end
+end
